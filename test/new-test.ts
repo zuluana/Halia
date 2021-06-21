ca8459898699543de50e53378abcf9c97a8e00fa
Oranda <hessiaapp@gmail.com>
2021-06-21 13:52:33 -0400

    linting fix

--- test/new-test.ts
+++ test/new-test.ts
@@ -4,45 +4,43 @@ export const AppPlugin: HaliaPlugin = {
   id: "app",
   name: "app",
   dependencies: [],
-  install: () => null
+  install: () => undefined
 };
 
 export const TabBarLayout: HaliaPlugin = {
   id: "layout",
   name: "layout",
   dependencies: [AppPlugin.id],
-  install: () => null
+  install: () => undefined
 };
 
 export const PluginPlugin: HaliaPlugin = {
   id: "plugin",
   name: "plugin",
   dependencies: [TabBarLayout.id],
-  install: () => null
+  install: () => undefined
 };
 
 export const SystemPlugin: HaliaPlugin = {
   id: "system",
   name: "system",
   dependencies: [TabBarLayout.id],
-  install: () => null
+  install: () => undefined
 };
 
 export const TaskPlugin: HaliaPlugin = {
   id: "task",
   name: "Task",
   dependencies: [PluginPlugin.id, TabBarLayout.id, AppPlugin.id],
-  install: () => null
+  install: () => undefined
 };
 
 describe("Halia", () => {
 
-  let haliaStack: HaliaStack;
-
   it("should successfully start the program", async () => {
 
     const haliaStack = new HaliaStack();
-    
+
     haliaStack.register(AppPlugin);
     haliaStack.register(TabBarLayout);
     haliaStack.register(PluginPlugin);
